b80735a713a578be8048d3b0f617b650e5f34f4c
stevenjmarsh <sjm.atl@gmail.com>
2016-03-17 18:10:52 -0400

    read env var

--- server/modules/set-environment-variables.js
+++ server/modules/set-environment-variables.js
@@ -2,6 +2,15 @@ let setEnvironmentVariables = () => {
   if ( Meteor.settings.private ) {
     process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
   }
+
+  //load api key from env var
+  let keyStr = process.env.YT_API_KEY;
+  if (keyStr) {
+    console.log('env var: ' + keyStr.slice(0, 3) + '...');
+  } else {
+    console.log('unable to read YK env variable');
+  }
+
 };
 
 Modules.server.setEnvironmentVariables = setEnvironmentVariables;
