e2899e9fb876b826826efc20ca40041aeaf6dd5b
stevenjmarsh <sjm.atl@gmail.com>
2016-03-19 15:43:47 -0400

    reduce bert alert display time even a little more

--- client/modules/initialize-client-settings.js
+++ client/modules/initialize-client-settings.js
@@ -1,7 +1,7 @@
 let initializeClientSettings = () => {
 
   // adjust default display time to 1.8 seconds (down from 3.5)
-  Bert.defaults.hideDelay = 1800;
+  Bert.defaults.hideDelay = 1400;
 
 };
 
